3ea201667901c4a546434c08ecdbc676887e4aaa
sholikul <sholikulardian@gmail.com>
2025-10-29 15:59:58 +0700

    chore: handle navigate

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,17 @@
 import type { Product } from '../types/product';
+import { useNavigate } from 'react-router-dom';
 
 export default function Card(product: Product) {
+  const navigate = useNavigate();
+
+  const handleNavigate = (id: number) => {
+    navigate(`/product/${id}`);
+  };
+
   return (
     <div
       key={product.id}
+      onClick={() => handleNavigate(product.id)}
       className='bg-card dark:bg-dark-card rounded-xl shadow cursor-pointer h-full flex flex-col justify-between'
     >
       <img src={product.images[0]} className='rounded-t-xl' />
