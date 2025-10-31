065c43a4dfb87f3bfcbd46e92b276322f3872826
= <=>
2025-10-31 16:28:52 +0700

    chore(page): add navigation to cart page

--- src/components/ToggleCart.tsx
+++ src/components/ToggleCart.tsx
@@ -1,10 +1,18 @@
 import { FaShoppingCart } from 'react-icons/fa';
 import useAuthStore from '../store/authStore';
 import useActiveCart from '../hooks/useActivecart';
+import { useNavigate } from 'react-router-dom';
 
 export default function ToggleCart() {
   const activeCart = useActiveCart();
   const currentUser = useAuthStore((state) => state.currentUser);
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    if (!currentUser) return navigate('/login');
+
+    navigate('/cart');
+  };
 
   return (
     <div className='relative cursor-pointer'>
@@ -13,7 +21,10 @@ export default function ToggleCart() {
           {activeCart.length}
         </div>
       )}
-      <FaShoppingCart className='text-2xl cursor-pointer' />
+      <FaShoppingCart
+        className='text-2xl cursor-pointer'
+        onClick={handleNavigate}
+      />
     </div>
   );
 }
