61c20332d538a018f9fe4e34bbe1105c26074e82
sholikul92 <sholikulardian@gmail.com>
2025-11-03 11:32:34 +0700

    chore: implement state delete item

--- src/pages/cart/CartPage.tsx
+++ src/pages/cart/CartPage.tsx
@@ -3,13 +3,15 @@ import useCartStore from "../../store/cartStore";
 import { useShallow } from "zustand/shallow";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { TiDeleteOutline } from "react-icons/ti";
 
 export default function CartPage() {
   const carts = useActiveCart();
-  const { increaseQuantityItem, decreaseQuantityItem } = useCartStore(
+  const { increaseQuantityItem, decreaseQuantityItem, deleteItem } = useCartStore(
     useShallow((state) => ({
       increaseQuantityItem: state.increaseQuantityItem,
       decreaseQuantityItem: state.decreaseQuantityItem,
+      deleteItem: state.deleteItem,
     }))
   );
   const [totalPrice, setTotalPrice] = useState<number>(0);
@@ -25,9 +27,7 @@ export default function CartPage() {
   return (
     <div className='mx-auto p-8'>
       <div className='grid grid-cols-[0.2fr_2fr_1fr_1fr] px-6 py-3 border-b text-textMuted dark:text-dark-textMuted font-medium text-sm'>
-        <div className=''>
-          <input type='checkbox' name='selectedAll' id='selectedAll' className='cursor-pointer' />
-        </div>
+        <div></div>
         <div>Product</div>
         <div className='text-center'>Quantity</div>
         <div className='text-right'>Price</div>
@@ -35,9 +35,8 @@ export default function CartPage() {
 
       {carts.map((item) => (
         <div key={item.id} className='grid grid-cols-[0.2fr_2fr_1fr_1fr] items-center px-6 py-4 border-b border-b-textMuted last:border-none'>
-          <div>
-            <input type='checkbox' name='selectedItem' id='selectedItem' className='cursor-pointer' />
-          </div>
+          <TiDeleteOutline className='cursor-pointer text-red-500 text-2xl' onClick={() => deleteItem(item.id)} />
+
           <div className='flex items-center gap-4'>
             <img src={item.imageUrl} alt={item.title} className='w-14 h-14 rounded-lg object-cover' />
             <div>
